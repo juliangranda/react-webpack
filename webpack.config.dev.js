9d7e0d3fdacf158e9a5138e7ab8218859969284b
juliangranda <juliangranda@hotmail.com>
2022-01-13 21:28:24 -0500

    dependencias de optimizacion

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMiniminizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-
 module.exports = {
     entry: './src/index.js', //El archivo fuente que se va ir a produccion
     output: {
         path: path.resolve(__dirname, "dist"), //como vamos a enviar nuestro recurso a produccion
-        filename: 'bundle.js',
-        publicPath:"/",
+        filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx'], //las extensiones que vamos a trabajar
-        alias:{
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles':path.resolve(__dirname, 'src/styles/'),
-        }
+        extensions: ['.js', '.jsx'] //las extensiones que vamos a trabajar
     },
-    mode: 'production',
+    mode: 'development',
     module: {
         rules: [
             {
@@ -52,15 +43,15 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        }),
-        new CleanWebpackPlugin(),
+        })
     ],
-    optimization:{
-        minimize:true,
-        minimizer:[
-            new CssMiniminizerPlugin(),
-            new TerserPlugin(),
-        ]
+    //servidor local para desarrollo
+    devServer: {
+        static: {
+        directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8080,
+        open: true,
     }
-
 }
